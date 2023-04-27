1f04a8a833cc304f4b6cc233bc9fe1471462fb29
Matt Megarry <mattmegarry@gmail.com>
2023-04-27 14:50:07 +0100

    secret key is loaded via environment variable

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -7,6 +7,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -16,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pp*b*k=w80awyec8&b2js32ge7y04=sz%nk%rw$qwct+$tnj4w'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
